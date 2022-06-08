88a2ebd014254064153cc2a8836908f36444dfbe
cdfortes <cdfortes@gmail.com>
2022-06-08 11:10:00 -0100

    global-css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-function App() {
+import './global.css'
+
+export function App() {
   return (
     <div className="App">
       <h1>Ignite Feed</h1>
     </div>
-  );
+  )
 }
-
-export default App;
